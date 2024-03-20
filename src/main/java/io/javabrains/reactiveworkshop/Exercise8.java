c7a3c0daacfe68afd3ed498046c696b49f291d88
TSO7099 <troythomas@shelterinsurance.com>
2024-03-20 07:32:13 -0500

    git commit -m "Error handling solutions."

--- src/main/java/io/javabrains/reactiveworkshop/Exercise8.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise8.java
@@ -12,14 +12,21 @@ public class Exercise8 {
         // Use ReactiveSources.intNumbersFluxWithException()
 
         // Print values from intNumbersFluxWithException and print a message when error happens
-        // TODO: Write code here
+        ReactiveSources.intNumbersFluxWithException()
+                .doOnError(error -> System.out.println("Error!" + error.getMessage()))    // Performs the action when an error occurs, but still passes the error further down the pipeline.
+                // By default, a subscription will end when an error occurs.
+                .subscribe(System.out::println);
 
         // Print values from intNumbersFluxWithException and continue on errors
-        // TODO: Write code here
+        ReactiveSources.intNumbersFluxWithException()
+                .onErrorContinue((error, item) -> System.out.println("Error!" + error.getMessage()))    // Catches the error and continues to pass emitted values down the pipeline
+                .subscribe(System.out::println);
 
         // Print values from intNumbersFluxWithException and when errors
         // happen, replace with a fallback sequence of -1 and -2
-        // TODO: Write code here
+        ReactiveSources.intNumbersFluxWithException()
+                .onErrorResume(error -> Flux.just(-1, -2))   // When an error is emitted, the error is caught and a new flux is passed down the pipeline
+                .subscribe(System.out::println);
 
         System.out.println("Press a key to end");
         System.in.read();
